123ce0147cf9bc8ffa3c395b91c2ce36b4190968
924461845@qq.com <cjx8852077>
2020-07-06 01:12:14 +0800

    Default Changelist

--- goroutine/goroutine.go
+++ goroutine/goroutine.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
 
+func main() {
+	var a [10]int
+	for i := 0; i < 10; i++ {
+		go func(i int) {
+			for {
+				a[i]++
+				runtime.Gosched()
+			}
+		}(i)
+	}
+	time.Sleep(time.Millisecond)
+	fmt.Println(a)
 }
